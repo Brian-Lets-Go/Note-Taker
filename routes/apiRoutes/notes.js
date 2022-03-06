09be78c7f7b59aa0ed7718ab8e65ab665713d084
brian-lets-go <brian.w.meenan@gmail.com>
2022-03-06 13:39:52 -0500

    commit for question

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { notes } = require('../../db/db');
+const { renderNotes } = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+    let note = notes;
+    res.json(note);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = renderNotes(req.body, notes);
+    res.json(note);
+})
+
+module.exports = router;
